5e1281035ab1af4b19a9b7d07ae1faab8583cf22
Shazahanul Islam Shohag <shohag121@gmail.com>
2022-12-28 11:18:46 +0600

    development environment setup with webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './examples',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
